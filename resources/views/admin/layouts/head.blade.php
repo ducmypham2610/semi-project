9080b6483c8b13e2e6cb0a6bada5a2219ec4ed6f
Hieu <hieunmbhaf200121@fpt.edu.vn>
2022-04-08 11:53:55 +0700

    changed links in head.blade.php

--- resources/views/admin/layouts/head.blade.php
+++ resources/views/admin/layouts/head.blade.php
@@ -6,10 +6,7 @@
 <title>Dashboard | BTEC shop</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{asset('css/styles.css')}}" rel="stylesheet">
 </head>
 <body>
